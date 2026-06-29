6e20f8f88fed71e73822e140720cb5da0effb056
Jenkins <jenkins@regula.by>
2026-06-29 18:16:23 +0300

    Release 9.6.18840

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["MRZRFID"]),
     ],
     targets: [
-        .binaryTarget(name: "MRZRFID", url: "https://pods.regulaforensics.com/MRZRFID/9.5.18276/DocumentReaderCore_mrzrfid_9.5.18276.zip", checksum: "adbb7bcc416d8d1aa513d17fc1b7e83444cc7e675ecdd5dc185b5eff9cdc0582"),
+        .binaryTarget(name: "MRZRFID", url: "https://pods.regulaforensics.com/MRZRFID/9.6.18840/DocumentReaderCore_mrzrfid_9.6.18840.zip", checksum: "84c969cd468e624c1bda746dbff3d48316114ab0d984ab9f8546cd5dca4bf400"),
     ]
 )
